aba84c719483002822a4fe7204da6288d54eef51
João Victor Santana Amorim <joaovictor520@hotmail.com>
2019-12-13 07:11:05 -0500

    Added animations and inpruved the controller to the mechanics

---
+++ Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform player;
+
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+
+
+
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -9,6 +9,11 @@ public class Controller : MonoBehaviour
     Rigidbody2D rb;
     SpriteRenderer renderer;
 
+    bool isGrounded;
+
+    [SerializeField]
+    Transform groundCheck;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +27,52 @@ public class Controller : MonoBehaviour
 
     private void FixedUpdate()
     {
+
+        if(Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            rb.velocity = new Vector2(2, 0);
+            rb.velocity = new Vector2(2, rb.velocity.y);
+            if(isGrounded)
+                animator.Play("Player_Run");
+            renderer.flipX = false;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                rb.velocity = new Vector2(5, rb.velocity.y);
+            }
         }        
         else if(Input.GetKey("a") || Input.GetKey("left"))
         {
-            rb.velocity = new Vector2(-2, 0);
+            rb.velocity = new Vector2(-2, rb.velocity.y);
+            if(isGrounded)
+                animator.Play("Player_Run");
+            renderer.flipX = true;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                rb.velocity = new Vector2(-5, rb.velocity.y);
+            }
         }
+        else
+        {
+            if(isGrounded)
+                animator.Play("Player_Idle");
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetKey("space") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 5);
+            animator.Play("Player_Jump");
+        }
+
     }
 
 }
